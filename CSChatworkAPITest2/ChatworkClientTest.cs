48fa1875dca82703618ae68ac32a302875feef2e
Masahiro Taniuchi <tany200ok+github@gmail.com>
2017-09-22 21:36:47 +0900

    modify add NUnit

---
+++ CSChatworkAPITest2/ChatworkClientTest.cs
@@ -0,0 +1,27 @@
+﻿using CSChatworkAPI;
+using NUnit.Framework;
+
+namespace CSChatworkAPITest2
+{
+    [TestFixture]
+    public class ChatworkClientTest
+    {
+        private ChatworkClient client = new ChatworkClient(TestContext.TestData.InputData.APIToken);
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCaseSource(typeof(TestCase.MeTestCase), nameof(TestCase.MeTestCase.TestCases))]
+        public void Test_Me(string chatworkId)
+        {
+            var me = client.GetMe();
+
+            Assert.IsNotNull(me);
+            Assert.IsNotEmpty(me.account_id);
+            Assert.IsNotEmpty(me.chatwork_id);
+            Assert.AreEqual(me.chatwork_id, chatworkId);
+        }
+    }
+}
